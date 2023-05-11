60f1fce0300d8d62ec107f25e5ffdc91c3baa486
Dantas_Dev02 <98811096+Gabr002@users.noreply.github.com>
2023-05-11 05:13:58 -0300

    Melhorias a cada dia de aprendizagem no Bootcamp.
    - O uso das tags HTML5.
    - Criação de tabelas.
    - Inserindo imagens.

--- site1/JS/script.js
+++ site1/JS/script.js
@@ -1,6 +1,11 @@
 // Nomenclatura
 function getValue(){
     var value = document.getElementById("name").value;
-    console.log(value);
     document.getElementById("result").innerHTML = "Bem vindo, " + value + "!";
+}
+
+function removeValue(){
+    document.getElementById("name").value = '';
+    document.getElementById("result").innerHTML = '';
+
 }
